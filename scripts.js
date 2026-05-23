185a5c95e76d7139f1a86f5ee8d73cd9ac66765a
Sebastian Caorsi <sebastiancaorsi3008@gmail.com>
2026-05-23 20:31:22 -0300

    Rework logic to play round with button input UI

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
-let randomNumber=Math.floor(Math.random()*3);
-
-function getComputerChoice(){
+//Function to get the randomized computer choice.
+function getComputerChoice(randomNumber){
 	
 	if(randomNumber==0){
 		return "Rock";
@@ -11,59 +10,60 @@ function getComputerChoice(){
 	}
 }
 
-let computerChoice; 
+//Initializes the game score at 0, could probably remove and it would still work
+let humanScore=0;
+let computerScore=0;
 
-function getHumanChoice(){
-	return prompt("Choose Rock, Paper or Scissors");
+//Function to play a round of rock, paper, scissors and log keep score
+function playRound(humanSelection, computerSelection){
+	if (humanSelection=="paper" && computerSelection=="rock"){
+		humanScore++;
+		console.log("Human Wins!");
+	}else if(humanSelection=="paper" && computerSelection=="scissors"){
+	    computerScore++;
+	    console.log("Computer Wins!");
+	}else if(humanSelection=="paper" && computerSelection=="paper"){
+	    console.log("It's a tie!");
+	}else if (humanSelection=="rock" && computerSelection=="scissors"){
+	    humanScore++;
+	    console.log("Human Wins!");
+	}else if(humanSelection=="rock" && computerSelection=="paper"){
+	    computerScore++;
+	    console.log("Computer Wins!");
+	}else if(humanSelection=="rock" && computerSelection=="rock"){
+	    console.log("It's a tie!");
+    }else if (humanSelection=="scissors" && computerSelection=="paper"){
+	    humanScore++;
+	    console.log("Human Wins!");
+	}else if(humanSelection=="scissors" && computerSelection=="rock"){
+	    computerScore++;
+	    console.log("Computer Wins!");
+	}else if(humanSelection=="scissors" && computerSelection=="scissors"){
+	    console.log("It's a tie!");
+    }
 }
 
-let humanChoice;
+//Each button plays a round and logs the winner and the current score
+//Math.floor(Math.random()*3) makes sure the number is 1, 2 or 3 and always integer
+const buttonRock = document.querySelector('#rock');
+buttonRock.addEventListener('click', () => {
+	playRound('rock', getComputerChoice(Math.floor(Math.random()*3)).toLowerCase());
+	console.log("Human: " + humanScore + " points.");
+	console.log("Computer: " + computerScore + " points.");
+});
 
-function playGame(){
+const buttonPaper = document.querySelector('#paper');
+buttonPaper.addEventListener('click', () => {
+	playRound('paper', getComputerChoice(Math.floor(Math.random()*3)).toLowerCase());
+	console.log("Human: " + humanScore + " points.");
+	console.log("Computer: " + computerScore + " points.");
+});
 
-    let humanScore=0;
-    let computerScore=0;
+const buttonScissors = document.querySelector('#scissors');
+buttonScissors.addEventListener('click', () => {
+	playRound('scissors', getComputerChoice(Math.floor(Math.random()*3)).toLowerCase());
+	console.log("Human: " + humanScore + " points.");
+	console.log("Computer: " + computerScore + " points.");
+});
 
-    function playRound(humanSelection, computerSelection){
-	    if (humanSelection=="paper" && computerSelection=="rock"){
-		    humanScore++;
-		    console.log("Human Wins!");
-	    }else if(humanSelection=="paper" && computerSelection=="scissors"){
-	    	computerScore++;
-	    	console.log("Computer Wins!");
-	    }else if(humanSelection=="paper" && computerSelection=="paper"){
-	    	console.log("It's a tie!");
-	    }else if (humanSelection=="rock" && computerSelection=="scissors"){
-	    	humanScore++;
-	    	console.log("Human Wins!");
-	    }else if(humanSelection=="rock" && computerSelection=="paper"){
-	    	computerScore++;
-	    	console.log("Computer Wins!");
-	    }else if(humanSelection=="rock" && computerSelection=="rock"){
-	    	console.log("It's a tie!");
-         }else if (humanSelection=="scissors" && computerSelection=="paper"){
-	    	humanScore++;
-	    	console.log("Human Wins!");
-	    }else if(humanSelection=="scissors" && computerSelection=="rock"){
-	    	computerScore++;
-	    	console.log("Computer Wins!");
-	    }else if(humanSelection=="scissors" && computerSelection=="scissors"){
-	    	console.log("It's a tie!");
-        }
-    }
-
-    for(let i=0; i<5; i++){
-        computerChoice = getComputerChoice(randomNumber).toLowerCase();
-        console.log("The computer chose " + computerChoice);
-
-        humanChoice= getHumanChoice().toLowerCase();
-        console.log("You chose " + humanChoice);
-        playRound(humanChoice, computerChoice);
-    }
-
-    console.log("Human: " + humanScore + " points.");
-    console.log("Computer: " + computerScore + " points.");
-
-}
-
-playGame();
+const scoreBoard = document.querySelector('.score');
